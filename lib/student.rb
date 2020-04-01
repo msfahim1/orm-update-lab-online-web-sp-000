964c7ed86243b945828c76df0f35526c27b65795
Michael Fahim <michael_fahim@yahoo.com>
2020-04-01 00:13:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,20 +30,14 @@ attr_reader :id
     DB[:conn].execute(sql)  
   end
   
- 
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
         VALUES (?, ?)
     SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql)  
   end
-
-  def self.create(name:, grade:)
-    student = Student.new(name, grade)
-    student.save
-    student
-  end
+  
   
 end
